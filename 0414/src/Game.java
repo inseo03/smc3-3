ecb3e8a5b26b9ce8fdbdaf8eb59a652cb898a1e9
BUGM <taegun0306@naver.com>
2021-04-14 15:52:42 +0900

    0414 동욱이 것으로 수정

--- 0414/src/Game.java
+++ 0414/src/Game.java
@@ -1,43 +1,63 @@
-import java.util.Random; 
 import java.util.Scanner;
-                           
+
 public class Game {
+
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);   
-		System.out.print("가위바위보 게임을 시작합니다.\n가위(1),바위(2),보(3),끝내기(4)");
-		String a=s.next(); 
-		int comN=(int)(Math.random()*4);  
-		int num=0;
+		Scanner scan = new Scanner(System.in);
 		
-		switch (a) {
-		case "1":
-			num=1;
-			break;
-		case "2":
-			num=2;
-			break;
-		case "3":
-			num=3;
-			break;
-			
-		case "4":
-			num=4;
-			break;
+		System.out.println("[1:가위  2:바위  3:보]");
+		
+		int user = scan.nextInt();
+		int com = (int)(Math.random()*3+1);
+		
+		Game(user,com);
+	}
+	
+	public static void Game(int user, int com){
+		switch(user){
+		case 1: System.out.print("[나: 가위!] VS");break;
+		case 2: System.out.print("[나: 바위!] VS");break;
+		case 3: System.out.print("[나: 보!] VS");break;
 		}
-		if(num==1 && comN==1||num==2 && comN==2||num==3 && comN==3) {
-			System.out.print("사용자는:"+num);
-			System.out.print("컴퓨터는:"+comN);
-			System.out.println("비겼습니다.");
-		}else if(num==1 && comN==2||num==2 && comN==3||num==3 && comN==1) {
-			System.out.print("사용자는:"+num);
-			System.out.print("컴퓨터는:"+comN);
-			System.out.println("졌습니다.");
-		}else if(num==1 && comN==3||num==2 && comN==1||num==3 && comN==2) {
-			System.out.println("사용자는: "+num);
-			System.out.println("컴퓨터는: "+comN);
-			System.out.println("이겼습니다.");
-		}else if(num==4) {
-			System.out.println("종료됩니다.");
+		switch(com){
+		case 1: System.out.println("[컴퓨터: 가위!]");break;
+		case 2: System.out.println("[컴퓨터: 바위!]");break;
+		case 3: System.out.println("[컴퓨터: 보!]");break;
+		}
+		
+		if(user == 1){
+			if(com == 1){
+				System.out.println("무승부...");
+			}
+			else if(com == 2){
+				System.out.println("컴퓨터 승!!!");
+			}
+			else if(com == 3){
+				System.out.println("사용자 승!!!");
+			}
+		}
+		else if(user == 2){
+			if(com == 1){
+				System.out.println("사용자 승!!!");
+			}
+			else if(com == 2){
+				System.out.println("무승부...");
+			}
+			else if(com == 3){
+				System.out.println("컴퓨터 승!!!");
+			}
+		}
+		else if(user == 3){
+			if(com == 1){
+				System.out.println("컴퓨터 승!!!");
+			}
+			else if(com == 2){
+				System.out.println("무승부...");
+			}
+			else if(com == 3){
+				System.out.println("사용자 승!!!");
+			}
+		}
+			
 		}
 	}
-}
