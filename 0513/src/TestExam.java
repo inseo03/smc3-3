3761fee35d99459a76bdf92bfab9af3e48adbe00
inseo <nivea0045@naver.com>
2021-05-13 15:08:41 +0900

    0513 프로젝트추가

---
+++ 0513/src/TestExam.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("500 이하의 자연수를 입력하세요.");
+		for(int i=0; i<= i++){
+			if(i%3==0|i%7==0){
+				
+			}
+		}
+		System.out.println(+"~"+"까지 3의 배수이면서 7의 배수인 수의 합은"+ +"입니다");
+
+	}
+
+}
